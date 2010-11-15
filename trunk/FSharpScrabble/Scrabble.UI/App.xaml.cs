bbf94d1fff117c159537d262272b963212130711
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2010-11-15 00:28:56 +0000

    Added a stubbed out implementation of my IGameWindow interface.

--- trunk/FSharpScrabble/Scrabble.UI/App.xaml.cs
+++ trunk/FSharpScrabble/Scrabble.UI/App.xaml.cs
@@ -5,12 +5,23 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
+using Scrabble.Core.Types;
+
 namespace Scrabble.UI
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Kicks off the game and initializes some Windows.
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            foreach (HumanPlayer p in Game.Instance.HumanPlayers)
+            {
+                SampleGameWindow w = new SampleGameWindow(p);
+                p.Window = w;
+                w.Show();
+            }
+        }
     }
 }
