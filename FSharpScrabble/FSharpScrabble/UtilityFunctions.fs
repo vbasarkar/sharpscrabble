f39b0097e576dea918860d5f5c99091324551e87
timothy.goric <timothy.goric@gmail.com>
2010-11-26 20:01:57 +0000

    MaximumScore utility function

--- FSharpScrabble/FSharpScrabble/UtilityFunctions.fs
+++ FSharpScrabble/FSharpScrabble/UtilityFunctions.fs
@@ -1,6 +1,10 @@
 ﻿module Scrabble.Core.UtilityFunctions
 
+open Scrabble.Core.Types
+
+
+let MaximumScore(tiles, move) = System.Convert.ToDouble(Move(move).Score)
+
 let SaveCommon(tiles, move) = 0.0
     
 let SmartSMoves(tiles, move) = 0.0
-    
