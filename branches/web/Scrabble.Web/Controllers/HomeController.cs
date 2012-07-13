d305d3c37769c1450b06b2a5d257a2ad0d9cf4c3
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2012-07-13 02:47:47 +0000

    Starting some of the game setup page/controller.

--- branches/web/Scrabble.Web/Controllers/HomeController.cs
+++ branches/web/Scrabble.Web/Controllers/HomeController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Scrabble.Web.Models;
 
 namespace Scrabble.Web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Play(ICollection<PlayerModel> players)
         {
             return View();
         }
---
+++ branches/web/Scrabble.Web/Models/PlayerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Scrabble.Core;
+
+namespace Scrabble.Web.Models
+{
+    public class PlayerModel
+    {
+        public String Name { get; set; }
+
+        public PlayerType Type { get; set; }
+
+        /// <summary>
+        /// Only for computer players.
+        /// </summary>
+        public String Provider { get; set; }
+
+        /// <summary>
+        /// Only for computer players.
+        /// </summary>
+        public String UtilityFunction { get; set; }
+    }
+}
