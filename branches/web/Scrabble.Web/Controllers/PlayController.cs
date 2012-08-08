2e1574e307bed8d6540f6729c72841a091bc4622
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2012-08-08 02:50:18 +0000

    Simple markup for the board, adding some files I missed before.

---
+++ branches/web/Scrabble.Web/Controllers/PlayController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scrabble.Core.Types;
+
+namespace Scrabble.Web.Controllers
+{
+    public class PlayController : Controller
+    {
+        public ActionResult Index(String id)
+        {
+            Board b = Game.Instance.PlayingBoard;
+            foreach (KeyValuePair<Core.Config.Coordinate, Core.Squares.Square> pair in b.OccupiedSquares())
+            {
+
+            }
+            return View("Index", (object)id);
+        }
+
+    }
+}
