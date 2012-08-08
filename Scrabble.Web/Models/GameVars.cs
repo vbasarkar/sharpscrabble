1c60ce1d257b9babff9ad4c335b8199a924dcd31
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2012-08-08 02:50:18 +0000

    Simple markup for the board, adding some files I missed before.

---
+++ Scrabble.Web/Models/GameVars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Scrabble.Core;
+
+namespace Scrabble.Web.Models
+{
+    public abstract class GameVars
+    {
+        private static WordLookup.WordLookup dictionary; 
+
+        public static void Initialize()
+        {
+            dictionary = new WordLookup.WordLookup(HttpContext.Current.Server.MapPath("~/App_Data/twl.txt"));
+        }
+
+        public static WordLookup.WordLookup DictionaryInstance()
+        {
+            return dictionary;
+        }
+    }
+}
---
+++ Scrabble.Web/Models/SessionGameLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scrabble.Core.Types;
+
+namespace Scrabble.Web.Models
+{
+    public class SessionGameLoader : IGameLoader
+    {
+        private static string KEY = "GAME_KEY";
+
+        public GameState Load()
+        {
+            object o = HttpContext.Current.Session[KEY];
+            if (o is GameState)
+                return (GameState)o;
+
+            throw new Exception("Game not in Session");
+        }
+
+        /// <summary>
+        /// Stores a GameState, and returns a unique identifier.
+        /// </summary>
+        /// <param name="state">The GameState to store.</param>
+        /// <returns></returns>
+        public String Put(GameState state)
+        {
+            HttpContext.Current.Session[KEY] = state;
+            String id = GenerateId();
+            //validate when the DB gets set up
+            return id;
+        }
+
+        private string GenerateId()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        } 
+    }
+}
