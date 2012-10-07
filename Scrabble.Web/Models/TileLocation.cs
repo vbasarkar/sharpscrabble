77c06c993561d795e7342221013f7f69f21f3898
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2012-10-07 19:33:14 +0000

    Working on posting turns to the server.

---
+++ Scrabble.Web/Models/TileLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrabble.Web.Models
+{
+    public class TileLocation
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Letter { get; set; }
+    }
+}
---
+++ Scrabble.Web/Models/TurnInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrabble.Web.Models
+{
+    public class TurnInput
+    {
+        public String Type { get; set; }
+        public TileLocation[] Tiles { get; set; }
+    }
+}
