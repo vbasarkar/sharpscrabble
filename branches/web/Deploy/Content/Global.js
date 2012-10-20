5609fad18101ae7ca7ce9d04d312f14b9ad52a80
wsanville <wsanville@e0084edf-4c47-530e-85bc-8f6083934ade>
2012-10-20 18:20:40 +0000

    Added a deploy folder.

---
+++ branches/web/Deploy/Content/Global.js
@@ -0,0 +1,604 @@
+﻿String.prototype.format = function ()
+{
+    var pattern = /\{\d+\}/g;
+    var args = arguments;
+    return this.replace(pattern, function (capture)
+    {
+        return args[capture.match(/\d+/)];
+    });
+}
+
+function createCookie(name, value, days)
+{
+    if (days)
+    {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name)
+{
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++)
+    {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function wtf()
+{
+    $('<div><p>An unexpected error has occured.</p><p>The page will be reloaded to attempt to continue the game.</p></div>').dialog(
+    {
+        modal: true,
+        title: 'Whoops',
+        buttons: 
+        [
+            { text: "Alright :'(", click: function () { window.location.href = window.location.href; } }
+        ]
+    });
+}
+
+function simpleDialog(message)
+{
+    $('<p>').text(message).dialog(
+    {
+        modal: true,
+        title: 'SharpScrabble',
+        buttons:
+        [
+            { text: 'Ok', click: function() { $(this).dialog('close'); } }
+        ]
+    });
+}
+
+function simpleConfirm(message, callback)
+{
+    $('<p>').text(message).dialog(
+    {
+        modal: true,
+        title: 'SharpScrabble',
+        buttons:
+        [
+            { text: 'Ok', click: function () { callback(); $(this).dialog('close'); } },
+            { text: 'Cancel', click: function () { $(this).dialog('close'); } }
+        ]
+    });   
+}
+
+function gameId()
+{
+    return readCookie('GameId');
+}
+
+/* Game implementation */
+var currentPlayerIndex = -1;
+
+var TurnTypes =
+{
+    Pass: 'Pass',
+    DumpLetters: 'DumpLetters',
+    PlaceMove: 'PlaceMove'
+};
+
+var turnMgr = (function ()
+{
+    var ok = false;
+    var turnInput = { Type: null, Tiles: [] };
+
+    return {
+        setCurrentTurn: function ()
+        {
+            ok = true;
+        },
+        commit: function ()
+        {
+            var r = this.reset;
+            if (ok && turnInput.Type)
+            {
+                disableButtons();
+                //Make a condensed version of the data to serialize to JSON.
+                var toSend = { Type: turnInput.Type, Tiles: [] }
+                for (var i in turnInput.Tiles)
+                {
+                    var info = turnInput.Tiles[i];
+                    toSend.Tiles.push({ X: info.X, Y: info.Y, Letter: info.Letter });
+                }
+                //Post this move to the server
+                invoker.setQueueMode();
+                $.ajax(
+                {
+                    type: 'POST',
+                    url: '/play/{0}/taketurn'.format(gameId()),
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    data: JSON.stringify(toSend),
+                    success: function (response)
+                    {
+                        if (response.IsValid)
+                        {
+                            console.log('Got response from post');
+                            ok = false;
+                            $.each(turnInput.Tiles, function (i, info)
+                            {
+                                if (info)
+                                    cloneTile(info.Element);
+                            });
+                            turnInput.Type = null;
+                            turnInput.Tiles = [];
+                        }
+                        else
+                        {
+                            enableButtons();
+                            simpleDialog('The move was not valid, please try again.');
+                            r();
+                        }
+                        invoker.setImmediateMode();
+                        invoker.runAll();
+                    },
+                    error: function (xhr)
+                    {
+                        if (xhr.status != 0)
+                            wtf();
+                    }
+                });
+            }
+        },
+        reset: function ()
+        {
+            //Needs to return any placed tiles to the player's rack.
+            $.each(turnInput.Tiles, function (i, t)
+            {
+                if (t)
+                {                    
+                    t.Element.data('square').removeClass('occupied');
+                    t.Element.data('square', null);
+                    var origin = t.Element.data('origin');
+                    t.Element.animate({ left: origin.left, top: origin.top }, 150);
+                }
+            });
+            turnInput.Type = null;
+            turnInput.Tiles = [];
+        },
+        tileDown: function (x, y, tile)
+        {
+            var letter = tile.attr('data-letter');
+            var index = tile.index();
+            console.log('{0} placed at ({1}, {2}). Tile index = {3}'.format(letter, x, y, tile.index()));
+            turnInput.Type = TurnTypes.PlaceMove;
+            turnInput.Tiles[index] = { X: x, Y: y, Letter: letter, Element: tile };
+        },
+        pass: function ()
+        {
+            turnInput.Type = TurnTypes.Pass;
+            turnInput.Tiles = [];
+            this.commit();
+        },
+        dumpLetters: function ()
+        {
+            turnInput.Type = TurnTypes.DumpLetters;
+            turnInput.Tiles = [];
+            this.commit();
+        },
+        log: function ()
+        {
+            console.log(turnInput);
+        }
+    };
+})();
+
+var invoker = (function ()
+{
+    var queueMode = false;
+    var queue = [];
+    var methods =
+    {
+        DrawTurn: function (message)
+        {
+            var wrapped = message.Payload;
+            if (wrapped.What === TurnTypes.PlaceMove)
+            {
+                var squares = [];
+                var needsPlacement = message.PlayerId != currentPlayerIndex; //to check if we already have the current player's tiles placed
+                $.each(wrapped.Payload, function (i, item)
+                {
+                    var s;
+                    if (needsPlacement)
+                        s = putTile(item.Key.X, item.Key.Y, item.Value);
+                    else
+                        s = findSquare(item.Key.X, item.Key.Y); 
+                    squares.push(s);
+                });
+ 
+                updateScore(message.PlayerId, wrapped.NewScore);
+                var row = showSummary(wrapped.Summary);
+                row.data('squares', squares).addClass('info').hover(summaryOver, summaryOut);
+            }
+            else
+            {
+                //Not much to do here, other than show the summary.
+                showSummary(wrapped.Summary);
+            }
+
+        },
+        GameOver: function (message)
+        {
+            showSummary('Game has ended! Finalising scores.');
+            $.each(message.Payload.AllPlayers, function (i, p)
+            {
+                //update score and tiles.
+                updateScore(p.Id, p.Score);
+                refreshPlayerTiles(p.Id, p.Tiles, false);
+            });
+            showWinners(message.Payload.Winners);
+        },
+        NotifyTurn: function (message)
+        {
+            if (isCurrentPlayer(message.PlayerId))
+            {
+                turnMgr.setCurrentTurn();
+                enableButtons();
+            }
+            else
+                disableButtons();
+        },
+        TilesUpdated: function (message)
+        {
+            refreshPlayerTiles(message.PlayerId, message.Payload, isCurrentPlayer(message.PlayerId));
+        },
+        Debug: function () { }
+    };
+
+    return {
+        add: function (raw)
+        {
+            var message = JSON.parse(raw);
+            if (message.What in methods)
+            {
+                var fn = (function (innerMessage)
+                {
+                    return function () { methods[innerMessage.What](innerMessage); }
+                })(message);
+                if (queueMode)
+                    queue.push(fn);
+                else
+                {
+                    this.runAll();
+                    fn();
+                }
+            }
+        },
+        runFirst: function ()
+        {
+            var first = queue.shift();
+            if (first)
+            {
+                first();
+                return true;
+            }
+            else
+                return false;
+        },
+        runAll: function ()
+        {
+            while (this.runFirst()) { };
+        },
+        setQueueMode: function ()
+        {
+            queueMode = true;
+        },
+        setImmediateMode: function ()
+        {
+            queueMode = false;
+        }
+    }
+})();
+
+function playerRack(who)
+{
+    return $('#player-{0} .rack'.format(who));
+}
+
+function updateScore(who, value)
+{
+    $('#player-{0} .playerScore'.format(who)).text(value);    
+}
+
+function showSummary(value)
+{
+    console.log(value);
+    var cssClass = consoleContainer.children().length % 2 == 0 ? 'entry' : 'entry alt';
+    var row = $('<div>').addClass(cssClass).html(value);
+    consoleContainer.prepend(row);
+    return row;
+}
+
+function summaryOver()
+{
+    $.each($(this).data('squares'), function (i, e)
+    {
+        $(e).addClass('highlight');
+    });
+}
+
+function summaryOut()
+{
+    $.each($(this).data('squares'), function (i, e)
+    {
+        $(e).removeClass('highlight');
+    });    
+}
+
+function showWinners(winners)
+{
+    if (!winners || winners.length == 0)
+    {
+        console.log('winners array was empty');
+        return;
+    }
+
+    var winningScore = winners[0].Score;
+    var humanWin = any(winners, function (p) { return isCurrentPlayer(p.Id); });
+    var hasHuman = hasHumanPlayer();
+    var d = $('<div>');
+    if (winners.length == 1)
+    {
+        if (humanWin)
+        {
+            $('<p>').addClass('winner').text('Congratulations {0}, you won!'.format(winners[0].Name)).appendTo(d);
+            $('<p>').text('Your final score was {0}, nice job!'.format(winningScore)).appendTo(d);
+        }
+        else
+        {
+            $('<p>').addClass('winner').text('{0} has won with a final score of {1}.'.format(winners[0].Name, winningScore)).appendTo(d);
+            if (hasHuman)
+                $('<p>').text('Better luck next time.').appendTo(d);   
+        }
+    }
+    else
+    {
+        $('<p>').addClass('winner').text(joinNames(winners) + ' have tied with a score of {0}!'.format(winningScore)).appendTo(d);
+        if (hasHuman)
+        {
+            if (humanWin)
+                $('<p>').text("You tied the computer, that's still pretty impressive!").appendTo(d);
+            else
+                $('<p>').text('Better luck next time.').appendTo(d);    
+        }
+    }
+    $('<p>').html("Check out the <a href='http://code.google.com/p/sharpscrabble/' target='_blank'>source code</a> if you're interested about how the game works!").appendTo(d);
+
+    $(d).dialog(
+    {
+        modal: true,
+        title: 'Game Over!',
+        buttons:
+        [
+            { text: 'New Game', click: function () { window.location.href = '/'; } },
+            { text: 'Ok', click: function () { $(this).dialog("close"); } }
+        ],
+        open: blurLinks
+    });
+}
+
+function any(array, fn)
+{
+    for (var i = 0, l = array.length; i < l; i++)
+    {
+        if (fn(array[i]))
+            return true;
+    }
+    return false;
+}
+
+function joinNames(players)
+{
+    if (!players || players.length == 0)
+        return '';
+
+    if (players.length == 1)
+        return players[0].Name;
+
+    var result = '';
+    var firstAdded = false;
+    var last = players.length - 1;
+    for (var i = 0; i < last; i++)
+    {
+        if (firstAdded)
+            result += ', ';
+        else
+            firstAdded = true;
+        result += players[i].Name;
+    }
+    result += ' and ' + players[last].Name;
+    return result;
+}
+
+function refreshPlayerTiles(who, tiles, canMove)
+{
+    var r = playerRack(who);
+    var baseLeft = r[0].offsetLeft;
+    var baseTop = r[0].offsetTop;
+    r.empty();
+    $.each(tiles, function (i, t)
+    {
+        var e = makeTile(t, canMove);
+        r.append(e);
+        positionTile(e, baseTop, baseLeft + 36 * i);
+    });
+}
+
+function makeTile(t, canMove)
+{
+    var t = $('<div>')
+        .addClass(canMove ? 'tile movable' : 'tile')
+        .text(t.Letter)
+        .attr('data-letter', t.Letter)
+        .append($('<span>').addClass('tileScore').text(t.Score));
+    return canMove ? movable(t) : t;
+}
+
+function positionRack(r)
+{
+    var baseLeft = r.offsetLeft;
+    var top = r.offsetTop;
+    $('.tile', r).each(function (i, e)
+    {
+        var left = baseLeft + 36 * i;
+        positionTile(e, top, left);
+    });
+}
+
+function positionTile(e, top, left)
+{
+    $(e).css('left', left).data('origin', { left: left, top: top });    
+}
+
+function movable(what)
+{
+    return what.draggable({ containment: '#playingArea', revert: 'invalid', revertDuration: 150, stack: '.tile' });
+}
+
+function isCurrentPlayer(who)
+{
+    return who == currentPlayerIndex;
+}
+
+function hasHumanPlayer()
+{
+    return currentPlayerIndex != -1;
+}
+
+function cloneTile(t)
+{
+    var clone = t.clone().removeClass('movable');
+    var target = t.data('square');
+    t.remove();
+    appendToSquare(clone, target);
+}
+
+function putTile(x, y, tile)
+{
+    //console.log('Putting {0} at ({1}, {2})'.format(tile.Letter, x, y));
+    var element = makeTile(tile, false);
+    var square = findSquare(x, y);
+    appendToSquare(element, square);
+    square.addClass('occupied');
+    return square;
+}
+
+function findSquare(x, y)
+{
+    return $('td:eq(' + x + ')', '#board tr:eq(' + y + ')');
+}
+
+function appendToSquare(e, square)
+{
+    $(e).appendTo(square.children().first());
+}
+
+function enableButtons()
+{
+    buttonArea.show();
+    $('button', buttonArea).button('enable');
+}
+
+function disableButtons()
+{
+    $('button', buttonArea).button('disable');
+}
+
+function blurLinks()
+{
+    $('a:first', $(this)).blur();
+}
+
+$(document).ready(function ()
+{
+    var about = $('#aboutDialog').dialog(
+    {
+        title: 'About SharpScrabble', autoOpen: false, modal: true, width: '450px', buttons:
+        [
+            { text: 'Cool Story, Bro', click: function () { $(this).dialog('close'); } }
+        ],
+        open: blurLinks
+    });
+    $('#about').click(function (e)
+    {
+        e.preventDefault();
+        about.dialog('open');
+    });
+    //Board setup
+    buttonArea = $('#buttonArea');
+    consoleContainer = $('#console .inner');
+    $('button').button();
+    $('#board td').droppable(
+    {
+        drop: function (event, ui)
+        {
+            $(this).removeClass('tile-over').addClass('occupied');
+            var oldSquare = ui.draggable.data('square');
+            if (oldSquare)
+                oldSquare.removeClass('occupied');
+            ui.draggable.data('square', $(this))
+            var x = $(this).index();
+            var y = $(this).parent().index();
+            turnMgr.tileDown(x, y, ui.draggable);
+            //snap to the square
+            ui.draggable.position(
+            {
+                my: 'top left',
+                at: 'to left',
+                of: $(this)
+            });
+        },
+        over: function ()
+        {
+            if (!$(this).hasClass('occupied'))
+                $(this).addClass('tile-over');
+        },
+        out: function ()
+        {
+            $(this).removeClass('tile-over');
+        },
+        accept: function ()
+        {
+            return !$(this).hasClass('occupied');
+        }
+    });
+    movable($('.movable'));
+    $('.rack').each(function (i, r)
+    {
+        positionRack(r);
+    });
+    //Button clicks
+    $('#done').click(function ()
+    {
+        turnMgr.commit();
+    });
+    $('#pass').click(function ()
+    {
+        simpleConfirm('Are you sure you want to pass?', function ()
+        {
+            turnMgr.pass();
+        });
+    });
+    $('#dumpLetters').click(function ()
+    {
+        simpleConfirm('Are you sure you want to exchange your tiles?', function ()
+        {
+            turnMgr.dumpLetters();
+        });
+    });
+    $('#reset').click(function ()
+    {
+        turnMgr.reset();
+    });
+})
---
+++ branches/web/Deploy/Content/Setup.js
@@ -0,0 +1,211 @@
+﻿(function ()
+{
+    var setupDialog, playerContainer, playerCount, radioCount, playerNameWatermark = 'Player Name';
+
+    function addPlayerRow(removable, human)
+    {
+        if (playerCount >= 4)
+            return;
+        removable = removable || false;
+        human = human || false;
+        var nameAttr = 'players[{0}].Name'.format(playerCount);
+        var row = $('<div>')
+            .addClass('row')
+            .addClass(human ? 'Human' : '')
+            .addClass(removable ? 'removable' : '')
+            .append(humanComputerPicker(playerCount, human))
+            .append($('<input>').attr('name', nameAttr).attr('type', 'text').focus(nameFocus).blur(nameBlur).addClass('watermark').val('Player Name'))
+            .append(moveGeneratorDropDown(playerCount))
+            .append(utilityFunctionDropDown(playerCount));
+        if (removable)
+            row.append(removeLink());
+        row.appendTo(playerContainer);
+        playerCount++;
+        checkEnableHuman();
+    }
+
+    function nameFocus()
+    {
+        if ($(this).hasClass('watermark'))
+            $(this).removeClass('watermark').val('');
+    }
+
+    function nameBlur()
+    {
+        if ($(this).val() == '')
+            $(this).addClass('watermark').val(playerNameWatermark);
+        else
+            $(this).removeClass('watermark');
+    }
+
+    function moveGeneratorDropDown(index)
+    {
+        return $('<select>')
+                    .addClass('com')
+                    .attr('name', 'players[{0}].Provider'.format(playerCount))
+                    .append($('<option>').attr('value', '').text('Select AI Type'))
+                    .append($('<option>').attr('value', '0').text('Brute Force (Strong)'))
+                    .append($('<option>').attr('value', '1').text('Hill Climber, 15 restarts (Medium)'))
+                    .append($('<option>').attr('value', '2').text('Hill Climber, 5 restarts (Weak)'));
+    }
+
+    function utilityFunctionDropDown(index)
+    {
+        return $('<select>')
+                    .addClass('com')
+                    .attr('name', 'players[{0}].UtilityFunction'.format(playerCount))
+                    .append($('<option>').attr('value', '').text('Select Utility Function'))
+                    .append($('<option>').attr('value', '0').text('Max Score'))
+                    .append($('<option>').attr('value', '1').text('Smart Plural Words'))
+                    .append($('<option>').attr('value', '2').text('Bingo Saver'))
+                    .append($('<option>').attr('value', '3').text('7 or More Letter Words'))
+                    .append($('<option>').attr('value', '4').text('5 or More Letter Words'))
+                    .append($('<option>').attr('value', '5').text('Bonus Square User'));
+    }
+
+    function removeLink()
+    {
+        return $('<a>').addClass('remove').click(function ()
+        {
+            playerCount--;
+            $(this).parent().remove();
+            checkEnableHuman();
+        });
+    }
+
+    function humanComputerPicker(index, isHuman)
+    {
+        var name = 'players[{0}].Type'.format(index);
+        var humanCheck = $('<input>').attr('type', 'radio').attr('id', 'radio' + radioCount).attr('name', name).val('Human');
+        var humanLabel = $('<label>').attr('for', 'radio' + radioCount).text('Human')
+        radioCount++;
+        var computerCheck = $('<input>').attr('type', 'radio').attr('id', 'radio' + radioCount).attr('name', name).val('Computer');
+        var computerLabel = $('<label>').attr('for', 'radio' + radioCount).text('Computer');
+        radioCount++;
+        (isHuman ? humanCheck : computerCheck).attr('checked', 'checked');
+        return $('<div>')
+                    .addClass('radiogroup')
+                    .addClass('noselect')
+                    .append(humanCheck)
+                    .append(humanLabel)
+                    .append(computerCheck)
+                    .append(computerLabel)
+                    .buttonset()
+                    .change(function ()
+                    {
+                        var cssClass = $('input:radio:checked', $(this)).val();
+                        $(this).parent().removeClass('Human Computer').addClass(cssClass);
+                        checkEnableHuman();
+                    });
+    }
+
+    function checkEnableHuman()
+    {
+        var selected = $('input:radio:checked[value="Human"]:first');
+        if (selected.length != 0)
+        {
+            $('input:radio[value="Human"]').each(function ()
+            {
+                if ($(this) != selected)
+                    $(this).button('disable');
+            });
+        }
+        else
+        {
+            $('.ui-buttonset input[value="Human"]').button('enable');
+        }
+    }
+
+    function validate()
+    {
+        var result = true;
+        playerContainer.children().each(function ()
+        {
+            var fn = $(this).hasClass('Human') ? validName : validComputerRow;
+            if (!fn($(this)))
+                result = false;
+        });
+        return result;
+    }
+
+    function validName(row)
+    {
+        var name = $('input:eq(0)', row).val();
+        return name != '' && name != playerNameWatermark;
+    }
+
+    function validComputerRow(row)
+    {
+        return validName(row) && $('select:eq(0)', row).val() != '' && $('select:eq(1)', row).val() != '';
+    }
+
+    function showError()
+    {
+        alert('Please fill out the Name for humans, and AI type for computers.');
+    }
+
+    $(document).ready(function ()
+    {
+        if (typeof WebSocket == 'undefined')
+        {
+            alert('Your browser does not support Web Sockets. We recommend using the latest version of Firefox or Chrome.');
+            return;
+        }
+
+        //Custom setup dialog
+        playerContainer = $('#playerContainer');
+        playerCount = 0;
+        radioCount = 0;
+        addPlayerRow(false, true);
+        addPlayerRow(false);
+        $('#addPlayer').click(function (e)
+        {
+            e.preventDefault();
+            addPlayerRow(true);
+        });
+
+        var okClick = function ()
+        {
+            if (validate())
+            {
+                $('#setupForm').submit();
+                $('button', setupDialog.parent()).button('disable');
+            }
+            else
+                showError();
+        };
+        setupDialog = $('#playerDialog').dialog(
+        {
+            width: 'auto', modal: true, autoOpen: false, title: 'Custom Setup', closeOnEscape: false, buttons: [{ text: 'Start Game', click: okClick}]
+        });
+
+        //Quick setup dialog
+        var quickName = $('#quickName').focus(function ()
+        {
+            if ($(this).hasClass('watermark'))
+                $(this).removeClass('watermark').val('');
+        }).blur(function ()
+        {
+            if ($(this).val() == '')
+                $(this).addClass('watermark').val('Your Name');
+            else
+                $(this).removeClass('watermark');
+        });
+        $('#quickStart').click(function (e)
+        {
+            e.preventDefault();
+            if (!quickName.hasClass('watermark'))
+                $('#quickStartForm').submit();
+        });
+        var quickDialog = $('#quickStartDialog').dialog(
+        {
+            width: '350px', modal: true, autoOpen: true, title: 'Welcome to SharpScrabble!', closeOnEscape: false, open: function () { quickName.blur(); }
+        });
+        $('#customLink').click(function (e)
+        {
+            e.preventDefault();
+            quickDialog.dialog('close');
+            setupDialog.dialog('open');
+        });
+    });
+})();
